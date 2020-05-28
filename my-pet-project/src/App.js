61c3477cd86487ce5f7af72161f8f121bc86d559
arvein <moonflower019@gmail.com>
2020-05-28 15:30:11 +0300

    add dialog component, refactor css modules

--- my-pet-project/src/App.js
+++ my-pet-project/src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
     return (
-        <div className="app-wrapper">
+        <div className='app-wrapper'>
             <Header/>
             <Sidebar/>
-            <Profile/>
+            <div className='app-wrapper-content'>
+                <Profile/>
+                {/*<Dialogs/>*/}
+            </div>
         </div>
     );
 }
