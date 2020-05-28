6718f302a79c8ef7a298d0961a9465bbfd41dca4
arvein <moonflower019@gmail.com>
2020-05-28 19:06:23 +0300

    refactor dialogs page, props

--- my-pet-project/src/components/Dialogs/Dialogs.jsx
+++ my-pet-project/src/components/Dialogs/Dialogs.jsx
@@ -1,19 +1,36 @@
 import React from "react";
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
+
+const DialogItem = ({id, name}) => {
+    const path = '/dialogs/' + id;
+
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={path} activeClassName={s.active}>{name}</NavLink>
+        </div>
+    )
+}
+
+const Message = ({message}) => {
+    return <div className={s.message}>{message}</div>
+}
 
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
-                <div className={s.dialog + ' ' + s.active}>NiKo</div>
-                <div className={s.dialog}>Dasha</div>
-                <div className={s.dialog}>Julia</div>
-                <div className={s.dialog}>Mark</div>
+
+                <DialogItem id='1' name='NiKo'/>
+                <DialogItem id='2' name='Dasha'/>
+                <DialogItem id='3' name='Julia'/>
+                <DialogItem id='4' name='Mark'/>
             </div>
+
             <div className={s.messages}>
-                <div className={s.message}>Hi.</div>
-                <div className={s.message}>Hi, how are you?</div>
-                <div className={s.message}>What is your status?</div>
+                <Message message='Hi.'/>
+                <Message message='Hi, how are you?'/>
+                <Message message='What is your status?'/>
             </div>
         </div>
     )
--- my-pet-project/src/components/Profile/MyPosts/MyPosts.jsx
+++ my-pet-project/src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,18 +5,20 @@ import Post from "./Post/Post";
 const MyPosts = () => {
     return (
         <div className={s.postsContainer}>
-            My Posts
+            <h3>My Posts</h3>
             <div className={s.postsContainer}>
                 <div>
                     <textarea>Enter post body</textarea>
-                    <button>Add Post</button>
-                    <button>Remove</button>
-                </div>
-                <div className='posts'>
-                   <Post message='Hi' likes='10'/>
-                   <Post message='My first post' likes='30'/>
-                   <Post message='What are you doing now?' likes='20'/>
-                   <Post message='Hi again' likes='100'/>
+                    <div>
+                        <button>Add Post</button>
+                        <button>Remove</button>
+                    </div>
+                    <div className={s.posts}>
+                        <Post message='Hi' likes='10'/>
+                        <Post message='My first post' likes='30'/>
+                        <Post message='What are you doing now?' likes='20'/>
+                        <Post message='Hi again' likes='100'/>
+                    </div>
                 </div>
             </div>
         </div>
