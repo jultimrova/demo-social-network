93dea35a7e341f7f06c8985c6a7287da9b2d1a78
arvein <moonflower019@gmail.com>
2020-06-03 22:05:38 +0300

    componentDidMount replace constructor

--- my-pet-project/src/components/Users/Users.jsx
+++ my-pet-project/src/components/Users/Users.jsx
@@ -4,11 +4,11 @@ import * as axios from 'axios'
 import users from './../../assets/images/users.jpg'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items);
-        })
+        });
     }
 
     render() {
