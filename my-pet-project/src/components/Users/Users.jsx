c6b2938ac4e2c28de793fee2186a35012279f9d4
arvein <moonflower019@gmail.com>
2020-06-03 21:49:23 +0300

    func component to class, users request

--- my-pet-project/src/components/Users/Users.jsx
+++ my-pet-project/src/components/Users/Users.jsx
@@ -1,60 +1,31 @@
 import React from 'react';
 import s from './Users.module.css';
+import * as axios from 'axios'
+import users from './../../assets/images/users.jpg'
 
-const Users = (props) => {
-
-    if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://i.pinimg.com/originals/c1/e8/b6/c1e8b6d72b7c16a3b1d4dd2006af9d7f.jpg',
-                    subscribe: true,
-                    fullName: 'Nikolai',
-                    status: 'hey hey hey',
-                    location: {city: 'Ucla', country: 'USA'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://i.pinimg.com/originals/c1/e8/b6/c1e8b6d72b7c16a3b1d4dd2006af9d7f.jpg',
-                    subscribe: true,
-                    fullName: 'Daria',
-                    status: 'go go go',
-                    location: {city: 'Kiev', country: 'Ukraine'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://i.pinimg.com/originals/c1/e8/b6/c1e8b6d72b7c16a3b1d4dd2006af9d7f.jpg',
-                    subscribe: true,
-                    fullName: 'Julia',
-                    status: 'me me me',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 4,
-                    photoUrl: 'https://i.pinimg.com/originals/c1/e8/b6/c1e8b6d72b7c16a3b1d4dd2006af9d7f.jpg',
-                    subscribe: false,
-                    fullName: 'Mark',
-                    status: 'la laa',
-                    location: {city: 'Kharkiv', country: 'Ukraine'}
-                },
-            ]
-        )
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items);
+        })
     }
 
-    return (
-        <div className={s.usersContainer}>
-            {props.users.map(u => <div key={u.id}>
+    render() {
+        return <div className={s.usersContainer}>
+            {this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img className={s.usersPhoto} src={u.photoUrl} alt=''/>
+                        <img className={s.usersPhoto} src={u.photos.small != null ? u.phatos.small : users}
+                             alt='userPhoto'/>
                     </div>
                     <div>
                         {u.subscribe
                             ? <button onClick={() => {
-                                props.unsubscribe(u.id)
+                                this.props.unsubscribe(u.id)
                             }}>UnSubscribe</button>
                             : <button onClick={() => {
-                                props.subscribe(u.id)
+                                this.props.subscribe(u.id)
                             }}>Subscribe</button>
                         }
 
@@ -62,17 +33,19 @@ const Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
+                        <div>{u.id}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
                     </span>
                 </span>
             </div>)}
         </div>
-    )
+
+    }
 }
 
 export default Users;
