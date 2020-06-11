656347490030b34303ad2f2f522d15070983ef0a
arvein <moonflower019@gmail.com>
2020-06-11 20:21:08 +0300

    create login redux-form

--- my-pet-project/src/components/Login/Login.jsx
+++ my-pet-project/src/components/Login/Login.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import {Field, reduxForm} from "redux-form";
 
-export const Login = (props) => {
+const LoginForm = (props) => {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={'Login'} name={'login'} component={'input'}/>
+            </div>
+            <div>
+                <Field placeholder={'Password'} name={'password'} component={'input'}/>
+            </div>
+            <div>
+                <Field component={'input'} name={'rememberMe'} type='checkbox'/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
     )
 }
 
-export default Login;
+const Login = (props) => {
+    const onSubmit = (formData) => {
+        console.log(formData);
+    }
+
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </div>
+}
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
+
+export default Login
