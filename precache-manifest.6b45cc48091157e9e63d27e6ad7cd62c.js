0c7dab3d36fece472dec834d72f582d191bf48f9
arvein <moonflower019@gmail.com>
2020-06-16 23:47:40 +0300

    Updates

---
+++ precache-manifest.6b45cc48091157e9e63d27e6ad7cd62c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e472ce068ad1ef6a6bfee0a8667867",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "bcf7c8b3bdc68540c468",
+    "url": "/social-network/static/css/main.4fad8fb4.chunk.css"
+  },
+  {
+    "revision": "c5259f07da0e1860e9fb",
+    "url": "/social-network/static/js/2.af92aab3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.af92aab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf7c8b3bdc68540c468",
+    "url": "/social-network/static/js/main.161bbe9c.chunk.js"
+  },
+  {
+    "revision": "11b9a8018df127179821",
+    "url": "/social-network/static/js/runtime-main.2c351028.js"
+  },
+  {
+    "revision": "46c9430bd8976f9780d614bc8818fe9d",
+    "url": "/social-network/static/media/preloader.46c9430b.svg"
+  },
+  {
+    "revision": "5d3f3632eb87e852f909b1004e9e7f18",
+    "url": "/social-network/static/media/usersPhoto.5d3f3632.jpg"
+  }
+]);
